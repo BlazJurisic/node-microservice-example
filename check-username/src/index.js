decd55380c4af75687805a13c77436266dcf8bd6
Blaz Jurisic <jurisic.blaz@gmail.com>
2018-04-25 19:29:36 +0200

    Update index.js
    Deep refactoring.

--- check-username/src/index.js
+++ check-username/src/index.js
@@ -1,26 +1,26 @@
-const config = require('./config');
+const config = require('./config')
 
-const got = require('got');
+const got = require('got')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/users/:username/check', function checkUsernameHandler(req, res) {
     checkIfExists(req.params.username)
         .then(exists => {
             if (exists) {
-                res.sendStatus(409);
+                res.sendStatus(409)
             } else {
-                res.sendStatus(204);
+                res.sendStatus(204)
             }
         });
 
     function checkIfExists(username) {
         return got(`http://${config.get('user.uri')}/users/${username}`)
-            .then(() => true, () => false);
+            .then(() => true, () => false)
     }
 });
 
-app.listen(config.get('port'));
+app.listen(config.get('port'))
